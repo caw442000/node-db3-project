a9a115384c6590266800e7bd7c3b37514ad9e6f7
cedric-winbush <cawinbushjr@gmail.com>
2020-03-18 14:52:48 -0500

    create find, findById and findSteps helpers

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -11,16 +11,24 @@ module.exports = {
 }
 
 function find() {
+  return db('schemes')
 
 }
 
 function findById(id) {
+  return db('schemes')
+    .where({ id })
+    .first();
 
 
 }
 
 function findSteps(id){
-
+  return db('steps')
+  .join('schemes as s', 'steps.scheme_id', 's.id' )
+  .select('steps.id','s.scheme_name','steps.step_number', 'steps.instructions' )
+    .where({ scheme_id : id })
+    .orderBy('step_number')
 }
 
 function add(scheme) {
@@ -34,5 +42,5 @@ function update(changes, id) {
 
 function remove(id){
 
-  
+
 }
