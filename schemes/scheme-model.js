bed0e4378063c2593ef7cc7ed8a590d6719050d3
cedric-winbush <cawinbushjr@gmail.com>
2020-03-18 13:58:38 -0500

    connect db to helper

---
+++ schemes/scheme-model.js
@@ -0,0 +1,38 @@
+const db = require('../data/db-config.js')
+
+module.exports = {
+
+  find,
+  findById, 
+  findSteps,
+  add,
+  update,
+  remove
+}
+
+function find() {
+
+}
+
+function findById(id) {
+
+
+}
+
+function findSteps(id){
+
+}
+
+function add(scheme) {
+
+}
+
+function update(changes, id) {
+
+
+}
+
+function remove(id){
+
+  
+}
